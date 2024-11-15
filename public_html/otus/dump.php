4df75ee2cbe0f92d494a543de316d0f5aadd5a3a
Fullofb3ans <igorgazinskii6@gmail.com>
2024-11-15 13:23:10 +0400

    fix

--- public_html/otus/dump.php
+++ public_html/otus/dump.php
@@ -1,10 +1,7 @@
 <?php 
-require $_SERVER['DOCUMENT_ROOT'] . '/bitrix/header.php';
-
-$APPLICATION -> setTitle('Дз №1');
 
 $now = new DateTime();
 
 \Bitrix\Main\Diag\Debug::dumpToFile($now, 'otusDz.log');
 
-require $_SERVER['DOCUMENT_ROOT'] . '/bitrix/footer.php';
+?>
