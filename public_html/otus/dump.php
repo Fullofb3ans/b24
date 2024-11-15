d5bcd184b86bba93c94c2b536375268070f4019a
Fullofb3ans <igorgazinskii6@gmail.com>
2024-11-15 11:57:02 +0400

    Дз №1

--- public_html/otus/dump.php
+++ public_html/otus/dump.php
@@ -1,6 +1,10 @@
 <?php 
 require $_SERVER['DOCUMENT_ROOT'] . '/bitrix/header.php';
 
-$APPLICATION -> setTitle('Пример отладки');
-\Bitrix\Main\Diag\Debug::dump([1, 2, 3, 4]);
+$APPLICATION -> setTitle('Дз №1');
+
+$now = new DateTime();
+
+\Bitrix\Main\Diag\Debug::dumpToFile($now, 'otusDz.log');
+
 require $_SERVER['DOCUMENT_ROOT'] . '/bitrix/footer.php';
