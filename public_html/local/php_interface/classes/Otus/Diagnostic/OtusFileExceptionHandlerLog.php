910fed69a95b4e12c02f486361b52d257fd7010c
Fullofb3ans <igorgazinskii6@gmail.com>
2024-11-15 13:01:49 +0400

    dz #2

--- public_html/local/php_interface/classes/Otus/Diagnostic/OtusFileExceptionHandlerLog.php
+++ public_html/local/php_interface/classes/Otus/Diagnostic/OtusFileExceptionHandlerLog.php
@@ -7,6 +7,16 @@ use Bitrix\Main\Diag\ExceptionHandlerFormatter;
 class OtusFileExceptionHandlerLog extends FileExceptionHandlerLog
 {
     public function write($exception, $logType){
-        
+        $text = ExceptionHandlerFormatter::format($exception, false, $this->level);
+
+		$context = [
+			'type' => static::logTypeToString($logType),
+		];
+
+		$logLevel = static::logTypeToLevel($logType);
+
+		$message = "OTUS {date} - Host: {host} - {type} - {$text}\n";
+
+		$this->logger->log($logLevel, $message, $context);
     }
 }
