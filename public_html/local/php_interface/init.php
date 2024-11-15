31123b3d832e508caf911ac9d9a187ebf825aec1
Fullofb3ans <igorgazinskii6@gmail.com>
2024-11-15 13:27:15 +0400

    fix #2?

--- public_html/local/php_interface/init.php
+++ public_html/local/php_interface/init.php
@@ -1,13 +1,13 @@
 <?php
-// define('DEBUG_FILE_NAME', $_SERVER["DOCUMENT_ROOT"] .'/logs/'.date("Y-m-d").'.log');
+define('DEBUG_FILE_NAME', $_SERVER["DOCUMENT_ROOT"] .'/logs/'.date("Y-m-d").'.log');
 
-// if(file_exists(__DIR__.'classes/autoload.php'))
-// {
-//     require_once __DIR__.'classes/autoload.php';
-// }
+if(file_exists(__DIR__ . '/classes/autoload.php'))
+{
+    require_once __DIR__ . '/classes/autoload.php';
+}
 
-// if(file_exists(__DIR__.'vendor/autoload.php'))
-// {
-//     require_once __DIR__.'vendor/autoload.php';
-// }
+if(file_exists(__DIR__ . '/vendor/autoload.php'))
+{
+    require_once __DIR__ . '/vendor/autoload.php';
+}
 ?>
