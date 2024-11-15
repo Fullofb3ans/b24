0545c80082a272d9f448daf1f8ca9f18e9abca54
Fullofb3ans <igorgazinskii6@gmail.com>
2024-11-15 12:54:34 +0400

    доп файлы с local из лекции

---
+++ public_html/local/php_interface/classes/Otus/Diagnostic/OtusFileExceptionHandlerLog.php
@@ -0,0 +1,12 @@
+<?php 
+namespace Otus\Diagnostic;
+
+use Bitrix\Main\Diag\FileExceptionHandlerLog;
+use Bitrix\Main\Diag\ExceptionHandlerFormatter;
+
+class OtusFileExceptionHandlerLog extends FileExceptionHandlerLog
+{
+    public function write($exception, $logType){
+        
+    }
+}
