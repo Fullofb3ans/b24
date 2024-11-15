800459c51154b97f79ac98646ebcf43667b80116
Fullofb3ans <igorgazinskii6@gmail.com>
2024-11-15 12:58:52 +0400

    Перебил init php

--- public_html/local/php_interface/init.php
+++ public_html/local/php_interface/init.php
@@ -1,3 +1,13 @@
 <?php
-echo 1;
+define('DEBUG_FILE_NAME', $_SERVER["DOCUMENT_ROOT"] .'/logs/'.date("Y-m-d").'.log');
+
+if(file_exists(__DIR__.'classes/autoload.php'))
+{
+    require_once __DIR__.'classes/autoload.php';
+}
+
+if(file_exists(__DIR__.'vendor/autoload.php'))
+{
+    require_once __DIR__.'vendor/autoload.php';
+}
 ?>
