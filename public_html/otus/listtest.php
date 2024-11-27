d6b2cdaf97b9059226e901f118c572bce71fa56d
Fullofb3ans <igorgazinskii6@gmail.com>
2024-11-27 11:41:29 +0400

    test vivod lista

--- public_html/otus/listtest.php
+++ public_html/otus/listtest.php
@@ -4,6 +4,18 @@ require $_SERVER['DOCUMENT_ROOT'] . '/bitrix/header.php';
 
 $APPLICATION -> setTitle('Тест пейдж');
 
+$iblockId = 16;
+$iblockElementId = 29;
+
+$arFilter = ['IBLOCK_ID' => $iblockId, 'ACTIVE'=> 'Y'];
+$arSelect = ['ID', 'NAME', 'CODE', 'PROPERTY_MODEL'];
+
+$res = CIBlockElement::getList([], $arfilter, false, [], $arSelect);
+while($arFields = $res-> fetch())
+{
+    pr($arFields);
+}
+
 
 require $_SERVER['DOCUMENT_ROOT'] . '/bitrix/footer.php';
 
