1473b0fc58e3059d2595387e49b76a2a037d956e
Fullofb3ans <igorgazinskii6@gmail.com>
2024-11-28 11:03:49 +0400

    patch autoloading

--- public_html/local/php_interface/init.php
+++ public_html/local/php_interface/init.php
@@ -1,6 +1,9 @@
 <?php
 define('DEBUG_FILE_NAME', $_SERVER["DOCUMENT_ROOT"] .'/logs/'.date("Y-m-d").'.log');
 
+include_once __DIR__ . '/../app/autoload.php';
+
+
 function pr($var, $type = false){
     echo '<pre style="font-size: 10px; border:1px solid #000; background: #FFF; text-align: left; color: #000;">';
     if($type)
