2748c052b80dbf833e04580d7295a340b50839b3
Fullofb3ans <igorgazinskii6@gmail.com>
2024-12-05 16:32:14 +0400

    test

--- public_html/local/app/Models/Lists/DocsProcedurePropertyValuesTable.php
+++ public_html/local/app/Models/Lists/DocsProcedurePropertyValuesTable.php
@@ -7,18 +7,5 @@ use Models\AbstractIblockPropertyValuesTable;
 class DocsProcedurePropertyValuesTable extends AbstractIblockPropertyValuesTable
 {
     const IBLOCK_ID = 17;
-
-    public static function getMap(): array
-    {
-        $map = [
-            'DOCTOR' => new ReferenceField(
-                'DOCTOR', 
-                DocsProcedurePropertyValuesTable::class, 
-                ['=this.DOCTOR_ID' => 'ref.IBLOCK_ELEMENT_ID']
-            )
-        ];
-
-        return parent::getMap() + $map; 
-
-    }
+    
 }
