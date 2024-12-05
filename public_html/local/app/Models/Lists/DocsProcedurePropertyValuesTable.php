0e97c3b5ad97f9c5084271f937fdc911af98bca8
Fullofb3ans <igorgazinskii6@gmail.com>
2024-12-05 16:27:31 +0400

    test

--- public_html/local/app/Models/Lists/DocsProcedurePropertyValuesTable.php
+++ public_html/local/app/Models/Lists/DocsProcedurePropertyValuesTable.php
@@ -7,4 +7,15 @@ use Models\AbstractIblockPropertyValuesTable;
 class DocsProcedurePropertyValuesTable extends AbstractIblockPropertyValuesTable
 {
     const IBLOCK_ID = 17;
+
+    public static function getMap()
+    {
+        return array_merge(parent::getMap(), [
+            'ID' => [
+                'data_type' => 'integer',
+                'primary' => true,
+                'autocomplete' => true,
+            ],
+        ]);
+    }
 }
