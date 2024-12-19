ae69e214d1e5382aec7fa5e804ee1a858ef686e8
Fullofb3ans <igorgazinskii6@gmail.com>
2024-12-19 16:32:56 +0400

    patch

--- public_html/local/php_interface/init.php
+++ public_html/local/php_interface/init.php
@@ -18,8 +18,8 @@ if(file_exists(__DIR__ . '/classes/autoload.php'))
     require_once __DIR__ . '/classes/autoload.php';
 }
 
-if(file_exists(__DIR__ . '../../vendor/autoload.php'))
+if(file_exists(__DIR__ . '/vendor/autoload.php'))
 {
-    require_once __DIR__ . '../../vendor/autoload.php';
+    require_once __DIR__ . '/vendor/autoload.php';
 }
 ?>
