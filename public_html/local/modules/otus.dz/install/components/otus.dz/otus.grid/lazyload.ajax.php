5c5c4b6d9c278a6cef59bfdb8f442898588e658a
Fullofb3ans <igorgazinskii6@gmail.com>
2024-12-26 10:05:15 +0400

    test

--- public_html/local/modules/otus.dz/install/components/otus.dz/otus.grid/lazyload.ajax.php
+++ public_html/local/modules/otus.dz/install/components/otus.dz/otus.grid/lazyload.ajax.php
@@ -28,6 +28,8 @@ if (!CModule::IncludeModule('crm') || !CCrmSecurityHelper::IsAuthorized() || !ch
 
 $dealId = (int)Application::getInstance()->getContext()->getRequest()->get('dealId');
 
+echo '<h1>TEST</h1>';
+
 $APPLICATION->IncludeComponent(
     'otus.dz:otus.grid',
     '.default',
