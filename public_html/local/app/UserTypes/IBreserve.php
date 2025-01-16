c1861d812860f159c6090a39a64a5feebd574e1f
Fullofb3ans <igorgazinskii6@gmail.com>
2025-01-16 11:34:15 +0400

    test

--- public_html/local/app/UserTypes/IBreserve.php
+++ public_html/local/app/UserTypes/IBreserve.php
@@ -6,6 +6,7 @@ use Models\Lists\DocsPropertyValuesTable as DocsTable;
 class IBreserve
 {
  private static function printProcedures(){
+    $doctorId = $arProperty['IBLOCK_ELEMENT_ID'];
     $docs = DocsTable::getList([      
         'select' => [
             'ID' => 'IBLOCK_ELEMENT_ID',
@@ -13,7 +14,7 @@ class IBreserve
             'PROCEDURA_ID',
         ],
             'filter' => [
-                'IBLOCK_ELEMENT_ID' => $arProperty['ELEMENT_ID']
+                'IBLOCK_ELEMENT_ID' => $doctorId
             ]
     ])->fetch();
 
