9bd28370af4135d3925c31d79d225c429e563e13
Fullofb3ans <igorgazinskii6@gmail.com>
2025-01-16 11:07:53 +0400

    test

--- public_html/local/app/UserTypes/IBreserve.php
+++ public_html/local/app/UserTypes/IBreserve.php
@@ -6,34 +6,32 @@ use Models\Lists\DocsPropertyValuesTable as DocsTable;
 class IBreserve
 {
  private static function printProcedures(){
-        $docs = DocsTable::getList([      
-            'select' => [
-                'ID' => 'IBLOCK_ELEMENT_ID',
-                'NAME' => 'ELEMENT.NAME',
-                'PROCEDURA_ID',
-            ],
-            'filter' => [
-                'IBLOCK_ELEMENT_ID' => $arProperty['ELEMENT_ID']
-            ]
-        ])->fetch();
-    
-        $proceduresAll = \Bitrix\Iblock\Elements\ElementProcTable::query()
-            ->addSelect('NAME')
-            ->addSelect('ID')
-            ->fetchCollection();
-    
-        $proceduraList = [];
-        foreach($proceduresAll as $procedura){
-            $proceduraList[$procedura->getId()] = $procedura->getName();
-        }
-    
-        $strResult = '<div class="procedures-text">';
+    $docs = DocsTable::getList([      
+        'select' => [
+            'ID' => 'IBLOCK_ELEMENT_ID',
+            'NAME' => 'ELEMENT.NAME',
+            'PROCEDURA_ID',
+        ]
+    ])->fetch();
+
+    $proceduresAll = \Bitrix\Iblock\Elements\ElementProcTable::query()
+        ->addSelect('NAME')
+        ->addSelect('ID')
+        ->fetchCollection();
+
+    $proceduraList = [];
+    foreach($proceduresAll as $procedura){
+        $proceduraList[$procedura->getId()] = $procedura->getName();
+    }
+
+    $strResult = '';
+    if ($docs && isset($docs['PROCEDURA_ID'])) {
         foreach($docs['PROCEDURA_ID'] as $procId) {
             $strResult .= $proceduraList[$procId] . '<br>';
         }
-        $strResult .= '</div>';
-    
-        return $strResult;
+    }
+
+    return $strResult;
     }
 
     public static function GetUserTypeDescription()
