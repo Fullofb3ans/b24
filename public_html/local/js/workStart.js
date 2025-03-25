a44bc369acf7b02cf4c57ccc393d21c1453cd166
Fullofb3ans <igorgazinskii6@gmail.com>
2025-03-25 10:06:27 +0400

    test

--- public_html/local/js/workStart.js
+++ public_html/local/js/workStart.js
@@ -1,29 +1,31 @@
+console.log('workStart.js loaded');
+
 BX.ready(function () {
-    console.log('ds')
-    BX.addCustomEvent('onPullEvent-timeman', function (command, params) {
-        if (command === 'start_day') {
-            var popup = new BX.PopupWindow("workday-popup", null, {
-                content: `
-                    <div style="padding: 20px;">
-                        <p>Вы уверены, что хотите начать рабочий день?</p>
-                        <button id="confirm-workday" class="ui-btn ui-btn-primary">
-                            Подтвердить начало дня
-                        </button>
-                    </div>
-                `,
-                closeIcon: true,
-                closeByEsc: true,
-                overlay: true
-            });
+    console.log('BX.ready executed');
 
-            popup.show();
+    BX.addCustomEvent('timeman:start', function (params) {
+        console.log('Timeman start event caught');
+        var popup = new BX.PopupWindow("workday-popup", null, {
+            content: `
+                <div style="padding: 20px;">
+                    <p>Вы уверены, что хотите начать рабочий день?</p>
+                    <button id="confirm-workday" class="ui-btn ui-btn-primary">
+                        Подтвердить начало дня
+                    </button>
+                </div>
+            `,
+            closeIcon: true,
+            closeByEsc: true,
+            overlay: true
+        });
 
-            BX.bind(BX('confirm-workday'), 'click', function () {
-                BX.timeman.chronometer.start();
-                popup.close();
-            });
+        popup.show();
 
-            return false;
-        }
+        BX.bind(BX('confirm-workday'), 'click', function () {
+            BX.timeman.chronometer.start();
+            popup.close();
+        });
+
+        return false;
     });
 });
